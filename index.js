d35ec032bd73ccd16e28e1c83501b0b7a6b0c03f
ThuBui <thu.bui@rikai.technology>
2021-01-09 13:54:05 +0900

    Implement translate

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.rules = [+2, -3];
+window.mode = "encode";
 
 $(document).ready(function () {
     for (var i = 0; i < window.rules.length; i++) {
         $(generateRuleHtml(window.rules[i])).insertBefore("#new-rule-input");
     }
 
-    // click button thêm quy luật
     $("#add-new-rule").click(function () {
         $("#new-rule-input").removeClass("d-none");
     });
@@ -22,9 +22,37 @@ $(document).ready(function () {
         }
     });
 
-    // Click button Dịch
+    $("[name=mode]").change(function (e) {
+        window.mode = e.target.value;
+    });
+
+    $("#translate").click(function () {
+        window.startTime = new Date();
+        switch (window.mode) {
+            case "encode":
+                break;
+            case "decode":
+                break;
+            default:
+                throw new Error("Not support");
+        }
+        window.endTime = new Date();
+
+        $("#start-time").html(window.startTime.getTime());
+        $("#end-time").html(window.endTime.getTime());
+        $("#steps-count").html(window.stepsCount);
+        
+    });
 });
 
 const generateRuleHtml = function (rule) {
     return '<button type="button" class="btn btn-outline-primary btn-rule">' + rule + '</button>';
-}
+};
+
+const encode = function () {
+    console.log("TODO: Encode");
+};
+
+const decode = function () {
+    console.log("TODO: decode");
+};
