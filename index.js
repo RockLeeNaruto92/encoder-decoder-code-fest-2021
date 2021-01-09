a1607ab4a19d2de9f10cd86c08335894311d2f5c
ThuBui <thu.bui@rikai.technology>
2021-01-09 13:32:51 +0900

    Add new rule

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 window.rules = [+2, -3];
 
 $(document).ready(function () {
-    // Display default refault rules
     for (var i = 0; i < window.rules.length; i++) {
-        $(generateRuleHtml(window.rules[i])).insertBefore("#add-new-rule");
+        $(generateRuleHtml(window.rules[i])).insertBefore("#new-rule-input");
     }
+
     // click button thêm quy luật
+    $("#add-new-rule").click(function () {
+        $("#new-rule-input").removeClass("d-none");
+    });
+
+    $("#new-rule-input").keydown(function (e) {
+        if (e.keyCode === 13) {
+            const value = parseInt($("#new-rule-input").val());
+            if (!window.rules.includes(value)) {
+                $("#new-rule-input").val("");
+                window.rules.push(value);
+                $(generateRuleHtml(value)).insertBefore("#new-rule-input");
+            }
+            $("#new-rule-input").addClass("d-none");
+        }
+    });
 
     // Click button Dịch
 });
