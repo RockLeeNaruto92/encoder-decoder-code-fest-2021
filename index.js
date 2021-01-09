eef4e8112a8207e5d4ec330bdbf1576917d3a2c1
ThuBui <thu.bui@rikai.technology>
2021-01-10 00:25:22 +0900

    encode ok

--- index.js
+++ index.js
@@ -4,13 +4,39 @@ window.data = {
     alphabet: '1ABC2DEF3GHI4JKL5MNO6PQRS7TUV8WXYZ90'.split(''),
     keyboard: {
         "1": "1",
+        "A": "ABC2",
+        "B": "ABC2",
+        "C": "ABC2",
         "2": "ABC2",
+        "D": "DEF3",
+        "E": "DEF3",
+        "F": "DEF3",
         "3": "DEF3",
+        "G": "GHI4",
+        "H": "GHI4",
+        "I": "GHI4",
         "4": "GHI4",
-        "5":  "JKL5",
-        "6":  "MNO6",
-        "7":  "PQRS7",
-        "8":  "TUV8",
+        "J": "JKL5",
+        "K": "JKL5",
+        "L": "JKL5",
+        "5": "JKL5",
+        "M": "MNO6",
+        "N": "MNO6",
+        "O": "MNO6",
+        "6": "MNO6",
+        "P": "PQRS7",
+        "Q": "PQRS7",
+        "R": "PQRS7",
+        "S": "PQRS7",
+        "7": "PQRS7",
+        "T": "TUV8",
+        "U": "TUV8",
+        "V": "TUV8",
+        "8": "TUV8",
+        "W": "WXYZ9",
+        "X": "WXYZ9",
+        "Y": "WXYZ9",
+        "Z": "WXYZ9",
         "9": "WXYZ9",
         "0": "0"
     }
@@ -42,7 +68,7 @@ $(document).ready(function () {
     });
 
     $("#translate").click(function () {
-        let contents = $("#code").val().split(".");
+        let contents = $("#code").val();
         let contentIndex = 0;
         let ruleIndex = 0;
         let resultStr = "";
@@ -51,9 +77,11 @@ $(document).ready(function () {
         window.numberResult = "";
         switch (window.mode) {
             case "encode":
-                encode(content);
+                contents = contents.split("");
+                window.result = encode(contents, contentIndex, window.rules, ruleIndex, resultStr);
                 break;
             case "decode":
+                contents = contents.split(".");
                 window.result = decode(contents, contentIndex, window.rules, ruleIndex, resultStr);
                 break;
             default:
@@ -62,7 +90,7 @@ $(document).ready(function () {
         window.endTime = new Date();
 
         $("#number-trans").html(window.numberResult);
-        $("#result").html(window.result);
+        $("#result").html(window.result.replaceAll("\n", "<br>"));
         $("#start-time").html(window.startTime.getTime());
         $("#end-time").html(window.endTime.getTime());
         $("#steps-count").html(window.stepsCount);
@@ -73,15 +101,7 @@ const generateRuleHtml = function (rule) {
     return '<button type="button" class="btn btn-outline-primary btn-rule">' + rule + '</button>';
 };
 
-const encode = function () {
-    console.log("TODO: Encode");
-    window.numberResult = "Encode number result";
-    window.result = "Encode result";
-    window.stepsCount = "sample";
-};
-
 const decode = function (contents, contentIndex, rules, ruleIndex, resultStr) {
-    console.log("TODO: decode");
     window.stepsCount += 1;
 
     if (contentIndex >= contents.length) {
@@ -97,21 +117,62 @@ const decode = function (contents, contentIndex, rules, ruleIndex, resultStr) {
 
     resultStr += decodeItem(contents[contentIndex], rules[ruleIndex]);
     
-    console.log(resultStr);
     let nextContentIndex = contentIndex + 1;
     let nextRuleIndex = (ruleIndex + 1) % rules.length;
     if (contents[contentIndex] === "\n") {
         nextRuleIndex = ruleIndex % rules.length;
     }
 
-    // apply rule for each content
     return decode(contents, nextContentIndex, rules, nextRuleIndex, resultStr);
 };
 
-const encodeItem = function (item, rule) {
-    
+const encode = function (contents, contentIndex, rules, ruleIndex, resultStr) {
+    window.stepsCount += 1;
 
-    return window.data.alphabet[(resIndexInKey + rule) % window.data.alphabet.length];
+    if (contentIndex >= contents.length) {
+        return resultStr;
+    }
+
+    const decodeItemRes = encodeItem(contents[contentIndex], rules[ruleIndex]);
+    if (decodeItemRes === "\n") {
+        resultStr += "\n";
+    } else {
+        if (resultStr.length === 0 || resultStr[resultStr.length - 1] === "\n" ) {
+            resultStr += decodeItemRes;
+        } else {
+            resultStr += "." + decodeItemRes;
+        }
+    }
+
+    let nextContentIndex = contentIndex + 1;
+    let nextRuleIndex = (ruleIndex + 1) % rules.length;
+
+    if (contents[contentIndex] === " ") {
+        nextRuleIndex = ruleIndex % rules.length;
+    }
+
+    return encode(contents, nextContentIndex, rules, nextRuleIndex, resultStr);
+};
+
+const encodeItem = function (item, rule) {
+    window.stepsCount += 1;
+    if (item === " ") {
+        window.numberResult += "<br>";
+        return "\n";
+    }
+    const resIndexInKey = window.data.alphabet.indexOf(item);
+    const baseCharacter = window.data.alphabet[(resIndexInKey - rule + window.data.alphabet.length) % window.data.alphabet.length];
+
+    window.numberResult += baseCharacter;
+    if (!window.data.keyboard[baseCharacter]) {
+        debugger;
+    }
+    const charactersList = window.data.keyboard[baseCharacter].split("");
+    const key = charactersList[charactersList.length - 1];
+    const index = charactersList.indexOf(baseCharacter) + 1;
+
+    return key + "-" + index;
+    
 };
 
 const decodeItem = function (item, rule) {
